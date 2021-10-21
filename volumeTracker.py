7f9f98717d4895778c557f7d40b267719c39ebf1
Bogdan <brasskazov@gmail.com>
2021-10-21 21:43:55 +0300

    V3.0

--- volumeTracker.py
+++ volumeTracker.py
@@ -58,8 +58,9 @@ curCoins = get_jsonOf_CurCoinsVolume()
 # to continue uncomment 48-63
 with open('db.txt') as json_file:
     savedCoins = json.load(json_file)
+
 for savedCoin in savedCoins:
-    for curCoin in curCoinsVol:
+    for curCoin in curCoins:
         if curCoin['symbol'] == savedCoin['symbol']:
             savedCoin['tracking1'] = savedCoin['tracking2']
             savedCoin['tracking2'] = savedCoin['tracking3']
@@ -74,33 +75,33 @@ for savedCoin in savedCoins:
             savedCoin['priceChangePercent'] = curCoin['priceChangePercent']
 
 with open('db.txt', 'w') as outfile:
-    json.dump(curCoins, outfile)
+    json.dump(savedCoins, outfile)
 
 # part 2 writing into exel
-# with open('db.txt') as json_file:
-#     coins = json.load(json_file)
+with open('db.txt') as json_file:
+    coins = json.load(json_file)
 
-# workbook = xlsxwriter.Workbook('volumeTracker.xlsx')
-# worksheet = workbook.add_worksheet('sheet')
-# row = 0
-# column = 0
+workbook = xlsxwriter.Workbook('volumeTracker.xlsx')
+worksheet = workbook.add_worksheet('sheet')
+row = 0
+column = 0
 
-# for i in coins:
-#     worksheet.write(row, 0, i['symbol'])
-#     worksheet.write(row, 1, float(i['tracking1']))
-#     worksheet.write(row, 2, float(i['tracking2']))
-#     worksheet.write(row, 3, float(i['tracking3']))
-#     worksheet.write(row, 4, float(i['tracking4']))
-#     worksheet.write(row, 5, float(i['tracking5']))
-#     worksheet.write(row, 6, float(i['tracking6']))
-#     worksheet.write(row, 7, float(i['tracking7']))
-#     worksheet.write(row, 8, float(i['tracking8']))
-#     worksheet.write(row, 9, float(i['tracking9']))
-#     worksheet.write(row, 10, float(i['tracking10']))
-#     worksheet.write(row, 12, float(i['priceChangePercent']))
-#     #addAndchange here   <>     and here  <>
-#     row+=1 
-# workbook.close()
+for i in coins:
+    worksheet.write(row, 0, i['symbol'])
+    worksheet.write(row, 1, float(i['tracking1']))
+    worksheet.write(row, 2, float(i['tracking2']))
+    worksheet.write(row, 3, float(i['tracking3']))
+    worksheet.write(row, 4, float(i['tracking4']))
+    worksheet.write(row, 5, float(i['tracking5']))
+    worksheet.write(row, 6, float(i['tracking6']))
+    worksheet.write(row, 7, float(i['tracking7']))
+    worksheet.write(row, 8, float(i['tracking8']))
+    worksheet.write(row, 9, float(i['tracking9']))
+    worksheet.write(row, 10, float(i['tracking10']))
+    worksheet.write(row, 12, float(i['priceChangePercent']))
+
+    row+=1 
+workbook.close()
 
 
 
